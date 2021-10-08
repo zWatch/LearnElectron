83ee5811620b3bd9821452fc77ad91113599889e
zWatch <z16c.nyj@gmail.com>
2021-10-08 14:21:37 +0800

    添加React Router

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,9 @@
 import ReactDOM from "react-dom";
 import { App } from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>, 
+  document.getElementById("root"));
